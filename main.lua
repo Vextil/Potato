69cbf528b570c2713df727e0cdc4584574023110
Joaquin Cuitiño <vextil@gmail.com>
2016-05-19 16:05:10 -0300

    Add more buttons, and text drawing

--- main.lua
+++ main.lua
@@ -1,15 +1,19 @@
+---- Created by: Joaquin Cuitiño ----
+
 debug = true
 dt = 0
+
+images = {}
 
 potatoes = { amount = 0, items = {} }
 buttons = { amount = 0, items = {} }
 
-game = { state = 0, round = 0, spawns = 0, spawnsLeft = 0 }
+game = { state = 0, round = 0, spawns = 0, spawnsLeft = 0, points = 0 }
 
 function love.load()
 	love.graphics.setNewFont("assets/fonts/K26CasualComics.ttf", 15)
-	potatoImg = love.graphics.newImage("assets/images/potato.png")
-	buttonImg = love.graphics.newImage("assets/images/button.png")
+	images.potato = love.graphics.newImage("assets/images/potato.png")
+	images.button = love.graphics.newImage("assets/images/button.png")
 	setState(state.main)
 end
 
@@ -32,7 +36,7 @@ end
 
 -------- STATES --------
 
--- I know, this looks like a weird implementation but there's a reason.
+-- Weird game state implementation.
 -- By changing states this way I can avoid repeating code like game.state = (current state)
 -- and removeAllButtons(), since they would have been needed in each state changing function.
 
@@ -41,7 +45,7 @@ state = { main = 1, playing = 2, typing = 3, success = 4, fail = 5 }
 stateFunctions = {
 
 	--[[MAIN]] function()
-		addButton(buttonImg, 300, 300, 0, function() setState(state.playing) end)
+		addButton(images.button, 300, 300, function() setState(state.playing) end)
 	end, 
 
 	--[[PLAYING]] function()
@@ -50,12 +54,30 @@ stateFunctions = {
 	end, 
 
 	--[[TYPING]] function()
+		-- Create keyboard to type potato counting guess
+		addButton(images.n0, 0, 0, function() inputWrite(1) end)
+		addButton(images.n1, 0, 0, function() inputWrite(1) end)
+		addButton(images.n2, 0, 0, function() inputWrite(1) end)
+		addButton(images.n3, 0, 0, function() inputWrite(1) end)
+		addButton(images.n4, 0, 0, function() inputWrite(1) end)
+		addButton(images.n5, 0, 0, function() inputWrite(1) end)
+		addButton(images.n6, 0, 0, function() inputWrite(1) end)
+		addButton(images.n7, 0, 0, function() inputWrite(1) end)
+		addButton(images.n8, 0, 0, function() inputWrite(1) end)
+		addButton(images.n9, 0, 0, function() inputWrite(1) end)
+		addButton(images.delete, 0, 0, function() inputDelete(1) end)
+		addButton(images.accept, 0, 0, function() inputAccept(1) end)
 	end,
 
 	--[[SUCCESS]] function()
+		game.points = game.points + game.spawns
+		game.spawns = 0
+		addButton(images.continue, 0, 0, function() setState(state.playing) end)
 	end,
 
 	--[[FAIL]] function()
+		game.points = 0
+		addButton(images.newGame, 0, 0, function() setState(state.playing) end)
 	end
 }
 
@@ -65,15 +87,18 @@ function setState(id)
 	stateFunctions[id]()
 end
 
--------- BUTTONS --------
+-------- TEXT -----------
 
-function buttonPressCheck(x, y)
-	for i , b in ipairs(buttons.items) do
-		if clicked(b, x, y) then
-			b.f()
-		end
-	end
+function drawText()
 end
+
+function addText(t, x, y)
+end
+
+function removeAllText()
+end
+
+-------- BUTTONS --------
 
 function drawButtons()
 	for i , b in ipairs(buttons.items) do
@@ -81,8 +106,8 @@ function drawButtons()
 	end
 end
 
-function addButton(i, x, y, r, f) 
-	b = { i = i, l = {x = x, y = y}, r = r, f = f }
+function addButton(i, x, y, f) 
+	b = { i = i, l = {x = x, y = y}, f = f }
 	table.insert(buttons.items, b)
 	buttons.amount = buttons.amount + 1
 end
@@ -90,6 +115,14 @@ end
 function removeAllButtons() 
 	for i = 1, buttons.amount, 1 do buttons.items[i] = nil end
 	buttons.amount = 0
+end
+
+function buttonPressCheck(x, y)
+	for i , b in ipairs(buttons.items) do
+		if clicked(b, x, y) then
+			b.f()
+		end
+	end
 end
 
 -------- POTATOES --------
@@ -116,7 +149,7 @@ function addPotato()
 	w, h, flags = love.window.getMode()
 	locations = getSpawnAndDestination(w, h, 400)
 	-- Potato has LOCATION, DESTINATION, ROTATION, ROTATION SPEED and IMAGE
-	potato = { l = locations.spawn, d = locations.destination, r = 0, rs = love.math.random(1, 10), i = potatoImg }
+	potato = { l = locations.spawn, d = locations.destination, r = 0, rs = love.math.random(1, 10), i = images.potato }
 	table.insert(potatoes.items, potato)
 	potatoes.amount = potatoes.amount + 1
 	game.spawnsLeft = game.spawnsLeft - 1
